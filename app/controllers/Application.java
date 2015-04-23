f8649c1d5935dc53032f0ded2a38afcb2ab5c58a
Carlos Lizcano <caldep@gmail.com>
2015-04-23 15:50:09 -0500

    Se reubican los métodos al contolador de Listados

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -29,47 +29,4 @@ public class Application extends Controller {
     }
     //-------------------------------------------------------------------------------
 
-    public static void show(Long id){
-        Listado listaMostrar = Listado.findById(id);
-        render(listaMostrar);
-    }
-    //--------------------------------------------------------------------------------
-    public static void guardarArticulo(Long idArticulo, String descripcion, Long idLista){
-
-        Articulo item = Articulo.findById(idArticulo);
-        Listado lista = Listado.findById(idLista);
-        lista.feModificacion = new Date();
-        //verifica que el articulo exista, sino, lo crea
-        if(item != null){
-            //modifica el artículo
-            item.descripcion = descripcion;
-            item.save();
-        }
-        else{
-            //agrega nuevo articulo
-            lista.addArticulo(descripcion);
-        }
-        lista.save();
-
-    }
-    //------------------------------------------------------------------------------------
-    public static void guardarListado(Long idLista, String nombre){
-
-        Listado lista = Listado.findById(idLista);
-
-        //verifica que la lista exista, sino, la crea
-        if(lista != null){
-            //modifica lista
-            lista.nombre = nombre;
-            lista.feModificacion = new Date();
-            lista.save();
-        }
-        else{
-            //agrega nueva lista
-            Listado newLista = new Listado(nombre);
-            newLista.save();
-        }
-
-    }
-    //--------------------------------------------------------------------------------------
 }
